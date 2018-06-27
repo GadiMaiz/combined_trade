95172ecc0694b39782251f38d6826fbc65a4cd0a
Udi Ilan <udi.ilan@bitmain.com>
2018-06-27 09:57:05 +0300

    Removing previous service before installing new service

--- install_bitmain_trade.bat
+++ install_bitmain_trade.bat
@@ -1,4 +1,5 @@
 vc_redist.x86.exe /install /quiet
+nssm remove "Bitmain Trade Service"
 nssm install "Bitmain Trade Service" bitmain_trade_service.exe "-p 5000"
 nssm set "Bitmain Trade Service" AppDirectory %CD%
 net start "Bitmain Trade Service"
